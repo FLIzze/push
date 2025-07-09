5a6489e5289fea81c69c23479ffc297a9bd53d79
flizze <alexandre.bel@ynov.com>
2025-07-09 20:15:12 +0200

    feat: Working websockets

--- client/src/player.ts
+++ client/src/player.ts
@@ -1,30 +1,30 @@
-import {Direction} from "../types.ts";
+import { Direction } from "../types.ts";
 
 export class Player {
     color: string;
     gravityStrength: number;
     grounded: boolean;
 
-    size: {width: number, height: number};
-    cords: {x: number; y: number};
-    velocity: {x: number; y: number};
+    size: { width: number, height: number };
+    cords: { x: number; y: number };
+    velocity: { x: number; y: number };
     friction: number;
-    terminalVelocity: {side: number, jump: number};
-    speed: {side: number, jump: number};
+    terminalVelocity: { x: number, y: number };
+    speed: { x: number, y: number };
     inputs: Set<Direction>;
 
-    constructor(size: {width: number, height: number}, color: string) {
+    constructor(size: { width: number, height: number }, color: string) {
         this.size = size;
         this.color = color;
 
         this.grounded = false;
-        this.speed = {side: 0.2, jump: -6};
-        this.terminalVelocity = {side: 8, jump: -10};
+        this.speed = { x: 0.2, y: -6 };
+        this.terminalVelocity = { x: 8, y: -10 };
         this.gravityStrength = 0.5;
         this.friction = 0.8;
 
-        this.cords = {x: 80, y: 0};
-        this.velocity = {x: 0, y: 0};
+        this.cords = { x: 80, y: 0 };
+        this.velocity = { x: 0, y: 0 };
         this.inputs = new Set<Direction.Left>();
     }
 
@@ -37,8 +37,8 @@ export class Player {
         if (!this.grounded) {
             this.velocity.y += this.gravityStrength;
 
-            if (this.velocity.y > Math.abs(this.terminalVelocity.jump)) {
-                this.velocity.y = Math.abs(this.terminalVelocity.jump);
+            if (this.velocity.y > Math.abs(this.terminalVelocity.y)) {
+                this.velocity.y = Math.abs(this.terminalVelocity.y);
             }
         }
 
@@ -54,16 +54,16 @@ export class Player {
 
     update() {
         if (this.inputs.has(Direction.Left)) {
-            this.velocity.x = Math.max(this.velocity.x - this.speed.side, -this.terminalVelocity.side);
+            this.velocity.x = Math.max(this.velocity.x - this.speed.x, -this.terminalVelocity.x);
         } else if (this.inputs.has(Direction.Right)) {
-            this.velocity.x = Math.min(this.velocity.x + this.speed.side, this.terminalVelocity.side);
+            this.velocity.x = Math.min(this.velocity.x + this.speed.x, this.terminalVelocity.x);
         } else {
             this.velocity.x *= this.friction;
             if (Math.abs(this.velocity.x) < 0.1) this.velocity.x = 0;
         }
 
         if (this.inputs.has(Direction.Up) && this.grounded) {
-            this.velocity.y = this.speed.jump;
+            this.velocity.y = this.speed.y;
             this.grounded = false;
         }
 
