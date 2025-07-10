df5c71788d61dd9d28833e905428d102badf4405
flizze <alexandre.bel@ynov.com>
2025-07-10 16:35:14 +0200

    feat: multiple sessions working! very ugly code for now

--- server/main.ts
+++ server/main.ts
@@ -3,23 +3,109 @@ import { WebSocketServer } from "ws";
 const PORT = 8080;
 const wss = new WebSocketServer({ port: PORT });
 
-// const players = [];
+type WsPlayersList = {
+    type: "playersList";
+    data: PlayerData[];
+}
+
+type PlayerData = {
+    id: string;
+    name: string;
+    size: { x: number, y: number };
+    cords: { x: number, y: number };
+    color: string;
+}
+
+export type WsConnect = {
+    type: "connect";
+    data: PlayerData;
+}
+
+// type WsDisconnect = {
+//     type: "disconnect";
+//     data: {
+//         id: string;
+//     };
+// }
+
+type WsCords = {
+    type: "cords";
+    data: PlayerInfo;
+}
+
+type ServerBroadcast = {
+    type: "broadcast";
+    data: PlayerInfo[];
+}
+
+type PlayerInfo = {
+    cords: { x: number, y: number };
+    id: string;
+}
+
+const players: PlayerData[] = [];
 
 wss.on("connection", (ws) => {
-    console.log("Client connected");
-
     ws.on("message", (message) => {
-        console.log(`Received from client: ${message.toString()}`);
+        const parsedMessage = JSON.parse(message.toString());
 
-        if (message.toString().startsWith("ping")) {
-            console.log("FIRST MESSAGE");
-            ws.send("pong");
+        switch (parsedMessage.type) {
+        case "connect":
+            const dataConnect: WsConnect = parsedMessage;
+
+            const playersList: WsPlayersList = {
+                type: "playersList",
+                data: players.map(player => ({
+                    id: player.id,
+                    name: player.name,
+                    size: player.size,
+                    cords: player.cords,
+                    color: player.color,
+                })),
+            };
+            ws.send(JSON.stringify(playersList));
+
+            players.push(dataConnect.data);
+
+            for (const client of wss.clients) {
+                if (client.readyState === client.OPEN) {
+                    client.send(JSON.stringify(dataConnect));
+                }
+            }
+            break;
+        case "cords":
+            const dataCords: WsCords = parsedMessage;
+            const player = players.find(player => player.id === dataCords.data.id);
+            if (player) {
+                player.cords = dataCords.data.cords;
+            }
+            break;
+        default:
+            console.log(`unknown type ${parsedMessage.type}`);
+            break;
         }
     });
 
     ws.on("close", () => {
-        console.log("Client disconnected");
+        console.log("NO DISCONNECTED NO");
     });
 });
+
+setInterval(() => {
+    const dataBroadcast: ServerBroadcast = {
+        type: "broadcast",
+        data: players.map(player => ({
+            id: player.id,
+            cords: player.cords,
+        })),
+    };
+
+    const message = JSON.stringify(dataBroadcast);
+    for (const client of wss.clients) {
+        if (client.readyState === client.OPEN) {
+            client.send(message);
+        }
+    }
+}, 1000 / 30);
 
 console.log(`WebSocket server running on ws://localhost:${PORT}`);
