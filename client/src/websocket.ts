a97880a9d1678322078b1acf2f86d809d4fb8273
flizze <bel.alexandre@proton.me>
2025-07-11 15:27:33 +0200

    refractor: Client/src/main.ts splitted
    splitted into
    client/src/websocket.ts
    client/src/utils/draw.ts
    client/src/handlers.ts
    
    Which should be way easier to manage

---
+++ client/src/websocket.ts
@@ -0,0 +1,62 @@
+import type { WsConnect, WsInputs } from "../../types/types.ts"
+import { handleBroadcast, handleConnect, handleDisconnect, handlePlayersList } from "./handlers.ts";
+import { player, players } from "./main.ts";
+
+const PORT = 8080;
+const ws = new WebSocket(`ws://localhost:${PORT}`);
+
+ws.onopen = () => {
+    players.set(player.id, player);
+
+    const data: WsConnect = {
+        type: "connect",
+        playerData: {
+            id: player.id,
+            size: player.size,
+            cords: player.cords,
+            color: player.color,
+        },
+    };
+
+    ws.send(JSON.stringify(data));
+};
+
+ws.onmessage = (event) => {
+    let parsedMessage: any;
+    try {
+        parsedMessage = JSON.parse(event.data);
+    } catch (err) {
+        console.error(`Error parsing message: ${err}`);
+        return;
+    }
+
+    switch (parsedMessage.type) {
+        case "broadcast":
+            handleBroadcast(parsedMessage, players);
+            break
+        case "connect":
+            handleConnect(parsedMessage, players);
+            break;
+        case "playersList":
+            handlePlayersList(parsedMessage, players);
+            break;
+        case "disconnect":
+            handleDisconnect(parsedMessage, players);
+            break;
+        default:
+            console.log(`unknown type ${parsedMessage.type}`);
+            break;
+    }
+};
+
+function sendInputs() {
+    if (ws.readyState === ws.OPEN) {
+        const wsInputs: WsInputs = {
+            type: "inputs",
+            inputs: Array.from(player.inputs),
+        }
+        ws.send(JSON.stringify(wsInputs));
+    }
+}
+
+export { sendInputs };
